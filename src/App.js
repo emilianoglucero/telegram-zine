b4c9cae5bf524814e1c1865f8c9693a157c2afb1
Emiliano <emiliano@MacBook-Pro-de-Emiliano.local>
2020-08-11 03:11:45 -0300

    add assets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+//import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import './App-mobile.css';
+import './App-mobile-galaxy.css';
 
 import bg1 from './assets/img/bg/bg1.png'
 import bg2 from './assets/img/bg/bg2.png'
@@ -11,10 +14,54 @@ import bg7 from './assets/img/bg/bg7.png'
 import bg8 from './assets/img/bg/bg8.png'
 import bg9 from './assets/img/bg/bg9.png'
 import bg10 from './assets/img/bg/bg10.png'
+import bg11 from './assets/img/bg/bg11.png'
 
 import screenshot1 from './assets/img/Screenshot1.png';
 import screenshot2 from './assets/img/Screenshot2.png';
 import screenshot3 from './assets/img/Screenshot3.png';
+import screenshot4 from './assets/img/Screenshot4.png';
+import screenshot5 from './assets/img/Screenshot5.png';
+import screenshot6 from './assets/img/Screenshot6.png';
+import screenshot7 from './assets/img/Screenshot7.png';
+import screenshot8 from './assets/img/Screenshot8.png';
+import screenshot9 from './assets/img/Screenshot9.png';
+import screenshot10 from './assets/img/Screenshot10.png';
+import screenshot11 from './assets/img/Screenshot11.png';
+import screenshot12 from './assets/img/Screenshot12.png';
+import screenshot13 from './assets/img/Screenshot13.png';
+import screenshot14 from './assets/img/Screenshot14.png';
+import screenshot15 from './assets/img/Screenshot15.png';
+import screenshot16 from './assets/img/Screenshot16.png';
+import screenshot17 from './assets/img/Screenshot17.png';
+import screenshot18 from './assets/img/Screenshot18.png';
+import screenshot19 from './assets/img/Screenshot19.png';
+import screenshot20 from './assets/img/Screenshot20.png';
+import screenshot21 from './assets/img/Screenshot21.png';
+import screenshot22 from './assets/img/Screenshot22.png';
+import screenshot23 from './assets/img/Screenshot23.png';
+import screenshot24 from './assets/img/Screenshot24.png';
+import screenshot25 from './assets/img/Screenshot25.png';
+import screenshot26 from './assets/img/Screenshot26.png';
+import screenshot27 from './assets/img/Screenshot27.png';
+import screenshot28 from './assets/img/Screenshot28.png';
+import screenshot29 from './assets/img/Screenshot29.png';
+import screenshot30 from './assets/img/Screenshot30.png';
+import screenshot31 from './assets/img/Screenshot31.png';
+import screenshot32 from './assets/img/Screenshot32.png';
+import screenshot33 from './assets/img/Screenshot33.png';
+import screenshot34 from './assets/img/Screenshot34.png';
+import screenshot35 from './assets/img/Screenshot35.png';
+import screenshot36 from './assets/img/Screenshot36.png';
+import screenshot37 from './assets/img/Screenshot37.png';
+import screenshot38 from './assets/img/Screenshot38.png';
+import screenshot39 from './assets/img/Screenshot39.png';
+import screenshot40 from './assets/img/Screenshot40.png';
+import screenshot41 from './assets/img/Screenshot41.png';
+import screenshot42 from './assets/img/Screenshot42.png';
+import screenshot43 from './assets/img/Screenshot43.png';
+import screenshot44 from './assets/img/Screenshot44.png';
+import screenshot45 from './assets/img/Screenshot45.png';
+import screenshot46 from './assets/img/Screenshot46.png';
 
 
 import audio1 from './assets/audios/01-1.ogg';
@@ -59,8 +106,18 @@ import audio214 from './assets/audios/37-214.ogg';
 import audio220 from './assets/audios/38-220.ogg';
 
 
-import video1 from './assets/videos/1-1.webm';
-import video2 from './assets/videos/1-2.webm';
+import video1 from './assets/videos/1.webm';
+import video2 from './assets/videos/1-1.webm';
+import video3 from './assets/videos/1-2.webm';
+import video4 from './assets/videos/1-3.webm';
+import video5 from './assets/videos/2-1.webm';
+import video6 from './assets/videos/2-2.webm';
+
+
+
+
+
+
 
 function Background(props) {
   return (   
@@ -130,6 +187,12 @@ const Background9 = {
 const Background10 = {
 
   imageUrl: bg10,
+  name: 'screenshot1'
+
+};
+const Background11 = {
+
+  imageUrl: bg11,
   name: 'screenshot1'
 
 };
@@ -214,14 +277,321 @@ const Screenshot3 = {
 
 };
 
+const Screenshot4 = {
+
+  imageUrl: screenshot4,
+  name: 'screenshot4',
+  id: "screenshot4"
+
+};
+
+const Screenshot5 = {
+
+  imageUrl: screenshot5,
+  name: 'screenshot5',
+  id: "screenshot5"
+
+};
+const Screenshot6 = {
+
+  imageUrl: screenshot6,
+  name: 'screenshot6',
+  id: "screenshot6"
+
+};
+const Screenshot7 = {
+
+  imageUrl: screenshot7,
+  name: 'screenshot7',
+  id: "screenshot7"
+
+};
+const Screenshot8 = {
+
+  imageUrl: screenshot8,
+  name: 'screenshot8',
+  id: "screenshot8"
+
+};
+const Screenshot9 = {
+
+  imageUrl: screenshot9,
+  name: 'screenshot9',
+  id: "screenshot9"
+
+};
+
+const Screenshot10 = {
+
+  imageUrl: screenshot10,
+  name: 'screenshot10',
+  id: "screenshot10"
+
+};
+const Screenshot11 = {
+
+  imageUrl: screenshot11,
+  name: 'screenshot11',
+  id: "screenshot11"
+
+};
+const Screenshot12 = {
+
+  imageUrl: screenshot12,
+  name: 'screenshot12',
+  id: "screenshot12"
+
+};
+const Screenshot13 = {
+
+  imageUrl: screenshot13,
+  name: 'screenshot13',
+  id: "screenshot13"
+
+};
+const Screenshot14 = {
+
+  imageUrl: screenshot14,
+  name: 'screenshot14',
+  id: "screenshot14"
+
+};
+const Screenshot15 = {
+
+  imageUrl: screenshot15,
+  name: 'screenshot15',
+  id: "screenshot15"
+
+};
+const Screenshot16 = {
+
+  imageUrl: screenshot16,
+  name: 'screenshot16',
+  id: "screenshot16"
+
+};
+const Screenshot17 = {
+
+  imageUrl: screenshot17,
+  name: 'screenshot17',
+  id: "screenshot17"
+
+};
+const Screenshot18 = {
+
+  imageUrl: screenshot18,
+  name: 'screenshot18',
+  id: "screenshot18"
+
+};
+const Screenshot19 = {
+
+  imageUrl: screenshot19,
+  name: 'screenshot19',
+  id: "screenshot19"
+
+};
+const Screenshot20 = {
+
+  imageUrl: screenshot20,
+  name: 'screenshot20',
+  id: "screenshot20"
+
+};
+const Screenshot21 = {
+
+  imageUrl: screenshot21,
+  name: 'screenshot21',
+  id: "screenshot21"
+
+};
+const Screenshot22 = {
+
+  imageUrl: screenshot22,
+  name: 'screenshot22',
+  id: "screenshot22"
+
+};
+const Screenshot23 = {
+
+  imageUrl: screenshot23,
+  name: 'screenshot23',
+  id: "screenshot23"
+
+};
+const Screenshot24 = {
+
+  imageUrl: screenshot24,
+  name: 'screenshot24',
+  id: "screenshot24"
+
+};
+
+const Screenshot25 = {
+
+  imageUrl: screenshot25,
+  name: 'screenshot25',
+  id: "screenshot25"
+
+};
+const Screenshot26 = {
+
+  imageUrl: screenshot26,
+  name: 'screenshot26',
+  id: "screenshot26"
+
+};
+const Screenshot27 = {
+
+  imageUrl: screenshot27,
+  name: 'screenshot27',
+  id: "screenshot27"
+
+};
+const Screenshot28 = {
+
+  imageUrl: screenshot28,
+  name: 'screenshot28',
+  id: "screenshot28"
+
+};
+const Screenshot29 = {
+
+  imageUrl: screenshot29,
+  name: 'screenshot29',
+  id: "screenshot29"
+
+};
+const Screenshot30 = {
+
+  imageUrl: screenshot30,
+  name: 'screenshot30',
+  id: "screenshot30"
+
+};
+const Screenshot31 = {
+
+  imageUrl: screenshot31,
+  name: 'screenshot31',
+  id: "screenshot31"
+
+};
+const Screenshot32 = {
+
+  imageUrl: screenshot32,
+  name: 'screenshot32',
+  id: "screenshot32"
+
+};
+const Screenshot33 = {
+
+  imageUrl: screenshot33,
+  name: 'screenshot33',
+  id: "screenshot33"
+
+};
+const Screenshot34 = {
+
+  imageUrl: screenshot34,
+  name: 'screenshot34',
+  id: "screenshot34"
+
+};
+const Screenshot35 = {
+
+  imageUrl: screenshot35,
+  name: 'screenshot35',
+  id: "screenshot35"
+
+};
+const Screenshot36 = {
+
+  imageUrl: screenshot36,
+  name: 'screenshot36',
+  id: "screenshot36"
+
+};
+const Screenshot37 = {
+
+  imageUrl: screenshot37,
+  name: 'screenshot37',
+  id: "screenshot37"
+
+};
+const Screenshot38 = {
+
+  imageUrl: screenshot38,
+  name: 'screenshot38',
+  id: "screenshot38"
+
+};
+const Screenshot39 = {
+
+  imageUrl: screenshot39,
+  name: 'screenshot39',
+  id: "screenshot39"
+
+};
+
+const Screenshot40 = {
+
+  imageUrl: screenshot40,
+  name: 'screenshot40',
+  id: "screenshot40"
+
+};
+
+const Screenshot41 = {
+
+  imageUrl: screenshot41,
+  name: 'screenshot41',
+  id: "screenshot41"
+
+};
+const Screenshot42 = {
+
+  imageUrl: screenshot42,
+  name: 'screenshot42',
+  id: "screenshot42"
+
+};
+const Screenshot43 = {
+
+  imageUrl: screenshot43,
+  name: 'screenshot43',
+  id: "screenshot43"
+
+};
+const Screenshot44 = {
+
+  imageUrl: screenshot44,
+  name: 'screenshot44',
+  id: "screenshot44"
+
+};
+const Screenshot45 = {
+
+  imageUrl: screenshot45,
+  name: 'screenshot45',
+  id: "screenshot45"
+
+};
+const Screenshot46 = {
+
+  imageUrl: screenshot46,
+  name: 'screenshot46',
+  id: "screenshot46"
+
+};
+
 
 
 
 
 function Video(props) {
   return (
-    <div className="draggable">
-      <video id={props.video.id} width={props.video.width} height={props.video.height} autoPlay loop>
+    <div className="draggable" id={props.video.id}>
+      <video className="videos" width={props.video.width} height={props.video.height} autoPlay muted playsInline loop>
         <source src={props.video.url} type="video/mp4"/>
       </video>
     </div>
@@ -235,7 +605,27 @@ const Video1 = {
 
 const Video2 = {
   url: video2,
-  id: "video1"
+  id: "video2"
+}
+
+const Video3 = {
+  url: video3,
+  id: "video3"
+}
+
+const Video4 = {
+  url: video4,
+  id: "video4"
+}
+
+const Video5 = {
+  url: video5,
+  id: "video5"
+}
+
+const Video6 = {
+  url: video6,
+  id: "video6"
 }
 
 
@@ -439,6 +829,8 @@ const Audio220 = {
 
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -454,13 +846,63 @@ function App() {
       <Background bg={Background8} />
       <Background bg={Background9} />
       <Background bg={Background10} />
+      <Background bg={Background11} />
 
       <Screenshot image={Screenshot1} />
       <Screenshot image={Screenshot2} />
       <Screenshot image={Screenshot3} />
+      <Screenshot image={Screenshot4} />
+      <Screenshot image={Screenshot5} />
+      <Screenshot image={Screenshot6} />
+      <Screenshot image={Screenshot7} />
+      <Screenshot image={Screenshot8} />
+      <Screenshot image={Screenshot9} />
+      <Screenshot image={Screenshot10} />
+      <Screenshot image={Screenshot11} />
+      <Screenshot image={Screenshot12} />
+      <Screenshot image={Screenshot13} />
+      <Screenshot image={Screenshot14} />
+      <Screenshot image={Screenshot15} />
+      <Screenshot image={Screenshot16} />
+      <Screenshot image={Screenshot17} />
+      <Screenshot image={Screenshot18} />
+      <Screenshot image={Screenshot19} />
+      <Screenshot image={Screenshot20} />
+      <Screenshot image={Screenshot21} />
+      <Screenshot image={Screenshot22} />
+      <Screenshot image={Screenshot23} />
+      <Screenshot image={Screenshot24} />
+      <Screenshot image={Screenshot25} />
+      <Screenshot image={Screenshot26} />
+      <Screenshot image={Screenshot27} />
+      <Screenshot image={Screenshot28} />
+      <Screenshot image={Screenshot29} />
+      <Screenshot image={Screenshot30} />
+      <Screenshot image={Screenshot31} />
+      <Screenshot image={Screenshot32} />
+      <Screenshot image={Screenshot33} />
+      <Screenshot image={Screenshot34} />
+      <Screenshot image={Screenshot35} />
+      <Screenshot image={Screenshot36} />
+      <Screenshot image={Screenshot37} />
+      <Screenshot image={Screenshot38} />
+      <Screenshot image={Screenshot39} />
+      <Screenshot image={Screenshot40} />
+      <Screenshot image={Screenshot41} />
+      <Screenshot image={Screenshot42} />
+      <Screenshot image={Screenshot43} />
+      <Screenshot image={Screenshot44} />
+      <Screenshot image={Screenshot45} />
+      <Screenshot image={Screenshot46} />
+      
+
 
       <Video  video={Video1}/>
       <Video  video={Video2}/>
+      <Video  video={Video3}/>
+      <Video  video={Video4}/>
+      <Video  video={Video5}/>
+      <Video  video={Video6}/>
 
       <Audio audio={Audio1} />
       <Audio audio={Audio21} />
